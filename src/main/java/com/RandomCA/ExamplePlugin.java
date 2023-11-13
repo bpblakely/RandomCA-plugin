a8d0fc6b47b24014cebbd7eba8a80ab414cb93b8
Brian <brianblakely1998@gmail.com>
2023-11-13 13:23:56 -0500

    Modified names

--- src/main/java/com/RandomCA/ExamplePlugin.java
+++ src/main/java/com/RandomCA/ExamplePlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.RandomCA;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,7 +14,7 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "RandomCA"
 )
 public class ExamplePlugin extends Plugin
 {
--- src/test/java/com/RandomCA/RandomCAPlugin.java
+++ src/test/java/com/RandomCA/RandomCAPlugin.java
@@ -1,9 +1,9 @@
-package com.example;
+package com.RandomCA;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class RandomCAPlugin
 {
 	public static void main(String[] args) throws Exception
 	{
